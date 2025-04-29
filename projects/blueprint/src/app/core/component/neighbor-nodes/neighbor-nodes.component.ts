0ae399625754bf0e8111beafa1971b960e419598
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2025-04-29 11:51:56 +0200

    fix duplicates in nearby view

--- projects/blueprint/src/app/core/component/neighbor-nodes/neighbor-nodes.component.ts
+++ projects/blueprint/src/app/core/component/neighbor-nodes/neighbor-nodes.component.ts
@@ -8,15 +8,15 @@ import { ColorUtil } from '../../utils/color-util';
 
 
 @Component({
-    selector: 'bp-neighbor-nodes',
-    imports: [CommonModule],
-    templateUrl: './neighbor-nodes.component.html',
-    styleUrl: './neighbor-nodes.component.scss',
-    animations: [
-        fadeInOut
-    ]
+  selector: 'bp-neighbor-nodes',
+  imports: [CommonModule],
+  templateUrl: './neighbor-nodes.component.html',
+  styleUrl: './neighbor-nodes.component.scss',
+  animations: [
+    fadeInOut
+  ]
 })
-export class NeighborNodesComponent implements OnChanges {
+export class NeighborNodesComponent {
 
   readonly graph = input.required<Graph>();
   readonly subject = input.required<string>();
@@ -27,19 +27,21 @@ export class NeighborNodesComponent implements OnChanges {
   public colorUtil = ColorUtil;
   public Number = Number;
 
-  subjectSignal = signal<string>('');
-  graphSignal = signal<Graph>(null);
+  // subjectSignal = signal<string>('');
+  //  graphSignal = signal<Graph>(null);
 
   currentNode: Signal<GraphNode | null> = computed(() => {
-    return this.graphSignal().nodes.find(node => node.id === this.subjectSignal()) ?? null;
+    return this.graph().nodes.find(node => node.id === this.subject()) ?? null;
   });
 
   nodeMap: Signal<Map<string, GraphNode[]>> = computed(() => {
-    if (this.graphSignal() === null) {
+    const graph = this.graph();
+    const subject = this.subject();
+    if (graph === null) {
       return new Map<string, GraphNode[]>();
     }
 
-    return this.buildNodeMap(this.subjectSignal(), this.graphSignal());
+    return this.#buildNodeMap(subject, graph);
   });
 
 
@@ -49,15 +51,8 @@ export class NeighborNodesComponent implements OnChanges {
     return keys;
   });
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['graph']) {
-      this.graphSignal.set(changes['graph'].currentValue);
-    } if (changes['subject']) {
-      this.subjectSignal.set(changes['subject'].currentValue);
-    }
-  }
 
-  private buildNodeMap(subject: string, graph: Graph): Map<string, GraphNode[]> {
+  #buildNodeMap(subject: string, graph: Graph): Map<string, GraphNode[]> {
 
     const subjectIsSourceLinks = graph.links.filter(link => link.source.id === subject);
     const subjectIsTargetLinks = graph.links.filter(link => link.target.id === subject);
@@ -66,6 +61,10 @@ export class NeighborNodesComponent implements OnChanges {
 
     subjectIsSourceLinks.forEach(link => {
       const nodes = nodeMap.get(link.target.type) || [];
+      // if node already exists in the array, do not push it again
+      if (nodes.find(node => node.id === link.target.id)) {
+        return;
+      }
       nodes.push(link.target);
       nodeMap.set(link.target.type, nodes);
     }
@@ -73,10 +72,15 @@ export class NeighborNodesComponent implements OnChanges {
 
     subjectIsTargetLinks.forEach(link => {
       const nodes = nodeMap.get(link.source.type) || [];
+      // if node already exists in the array, do not push it again
+      if (nodes.find(node => node.id === link.source.id)) {
+        return;
+      }
       nodes.push(link.source);
       nodeMap.set(link.source.type, nodes);
     });
 
+    console.log('nodeMap', nodeMap);
     return nodeMap;
   }
 
