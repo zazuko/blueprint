edccb27913f51886aed94a5faf79a2565810b918
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2025-05-22 16:04:33 +0200

    remove unused import

--- projects/blueprint/src/app/features/explore/service/graph/graph.service.ts
+++ projects/blueprint/src/app/features/explore/service/graph/graph.service.ts
@@ -6,7 +6,7 @@ import { QueryBuilderService } from '../query-builder/query-builder.service';
 
 import { Graph, RdfUiGraphNode, RdfUiLink } from '../../../../core/component/graph/model/graph.model';
 
-import { flux, rdf, rdfs } from '@blueprint/ontology';
+import { flux, rdf } from '@blueprint/ontology';
 import { rdfEnvironment } from 'projects/blueprint/src/app/core/rdf/rdf-environment';
 
 export interface QueryInput {
